3308391bb4b9ac851c732bdbc25cb5cf400ad422
Ninnja254 <quaypeter@yahoo.com>
2025-09-04 18:26:58 +0300

    doneeee 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,7 +5,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(2):
         a.append(tuple_a[i] if len(tuple_a) > i else 0)
         b.append(tuple_b[i] if len(tuple_b) > i else 0)
-    print(a)
-    print(b)
 
     return (a[0] + b[0], a[1] + b[1])
