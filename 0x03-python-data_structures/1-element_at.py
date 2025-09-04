6dc06cd449e68f76a8eb14a6af97c9b5ba1f781f
Ninnja254 <quaypeter@yahoo.com>
2025-09-04 16:40:43 +0300

    2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    res = None
+def element_at(my_list, idx, element):
     if (idx >= 0 and idx < len(my_list)):
-        res = my_list[idx]
-    return res
+        my_list[idx] = element
+    return my_list
