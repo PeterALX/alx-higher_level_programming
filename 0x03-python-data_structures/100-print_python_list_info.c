2ec467dba93757f7c2dde3f77aaf2ff273bf3fc2
Ninnja254 <quaypeter@yahoo.com>
2025-09-05 20:51:54 +0300

    done 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <Python.h>
+void print_python_list_info(PyObject *p) {
+	PyListObject *pAsList =  (PyListObject *)p;
+	printf("[*] Size of the Python List = %zd\n", Py_SIZE(p));
+	printf("[*] Allocated = %zd\n", pAsList->allocated);
+
+	for (Py_ssize_t i = 0; i < Py_SIZE(p); i++){
+		printf("Element %zd: %s\n", i ,Py_TYPE(pAsList->ob_item[i])->tp_name);
+	}
+}
