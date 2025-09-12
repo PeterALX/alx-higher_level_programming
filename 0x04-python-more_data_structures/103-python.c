c289aa57b8634eed3113cd703fa62a9cbe3db217
Ninnja254 <quaypeter@yahoo.com>
2025-09-12 18:37:32 +0300

    done?

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -8,13 +8,28 @@ void print_python_bytes(PyObject *p) {
   PyBytesObject *pAsBytesObj = (PyBytesObject *)p;
   Py_ssize_t size = pAsBytesObj->ob_base.ob_size;
   char *sval = pAsBytesObj->ob_sval;
+	int bytesToPrint = size + 1 < 10 ? size + 1 : 10;
 
   printf("  size: %zd\n", size);
   printf("  trying string: %s\n", sval);
-  printf("  first %zd bytes:", size + 1);
-  for (int i = 0; i < size + 1; i++) {
+  printf("  first %d bytes:", bytesToPrint);
+  for (int i = 0; i < bytesToPrint; i++) {
     printf(" %02x", (unsigned char)sval[i]);
   }
   printf("\n");
 }
-void print_python_list(PyObject *p) { printf("in biz: %p\n", (void *)p); }
+void print_python_list(PyObject *p) {
+	PyListObject *pAsList =  (PyListObject *)p;
+	Py_ssize_t size = pAsList->ob_base.ob_size;
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", pAsList->allocated);
+
+	for (Py_ssize_t i = 0; i < size; i++){
+		const char *type = pAsList->ob_item[i]->ob_type->tp_name;
+		printf("Element %zd: %s\n", i ,type);
+		if (strcmp("bytes", type) == 0) {
+			print_python_bytes(pAsList->ob_item[i]);
+		}
+	}
+}
