5033fc3e889c40eda2617431aec80a99f95b0c82
Ninnja254 <quaypeter@yahoo.com>
2025-10-03 17:36:13 +0300

    first attempt magicClass

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,13 +4,17 @@ import math
 
 
 class MagicClass:
+    """An implementation of the MagicClass disassembly"""
+
     def __init__(self, radius=0):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
+        """Returns the area of the square instance"""
         return self.__radius ** 2 * math.pi
 
     def circumfrence(self):
+        """Returns the circumfrence of the square instance"""
         return 2 * math.pi * self.__radius
